ab650b6213a54ea646b004f6ee35a6fd68609b9b
elias-git <lemkademelias@gmail.com>
2025-03-07 15:10:18 +0100

    message d'accueil

---
+++ to-do.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Bienvenue dans ta to-do list."
+echo "1) Ajouter une tâche."
+echo "2) Supprimer une tâche."
+echo "3) Afficher une tâche."
+echo "4) Quitter."
+
+read -p "Choississez une option: " choix
